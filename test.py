8c7abc1592adc4447aab089e80b17deee827d767
lil-boi <brunowlod@gmail.com>
2021-04-18 22:19:57 +0200

    dane przed wykonanie petli, lista panstw-help

--- test.py
+++ test.py
@@ -1,26 +1,45 @@
 import requests
-import random
 
 baseUrl = "https://restcountries.eu/rest/v2/"
-do ="yes"
 
-while do =="yes":
+do = "yes"
+while do == "yes":
     print("what info would you like to have :\n")
     print("1 Population \n2 Languages \n3 Timezones\n")
     choice = int(input("-->"))
+
+    question = input("would you like to get a country list? y/n: \n")
+    if question == "y":
+        firstLetter = input("podaj pierwszą literę szukanego panstwa:")
+        rList = requests.get(baseUrl)
+        jsonChoice = rList.json()
+        choiceList = []
+        counter = 0
+        for c in jsonChoice:
+            choiceList.append(c["name"])
+        for country in choiceList:
+            if country[0] == firstLetter:
+                print(country)
     countryChoice = input("what country you want that information for? \n")
+
+    params = {"fields": "population;languages;timezones", "fullText": "true"}
+    r = requests.get(baseUrl + f"name/{countryChoice}", params=params)
+    json_response = r.json()
+    #print(json_response)
+    countryChoice = json_response[0]
+
     if choice == 1:
-        params = {"fields": "population", "fullText": "true"}
-        r= requests.get(baseUrl + f"name/{countryChoice}", params=params)
-        print(r.text)
+        population = countryChoice['population']
+        print(f'population is {population}')
     elif choice == 2:
-        params = {"fields": "languages", "fullText": "true"}
-        r= requests.get(baseUrl + f"name/{countryChoice}", params=params)
-        print(r.text)
+        langList = []
+        for lang in countryChoice["languages"]:
+            langList.append(lang["nativeName"])
+        print(f"native language: {''.join(langList)}")
     else:
-        params = {"fields": "timezones", "fullText": "true"}
-        r= requests.get(baseUrl + f"name/{countryChoice}", params=params)
-        print(r.text)
+        timezones = countryChoice["timezones"]
+
+        print(f"timezones: {timezones}")
     end = input("want more info? y/n\n")
     if end != "y":
         break
